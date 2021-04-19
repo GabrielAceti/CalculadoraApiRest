9b55b704e9f89d4023a6aad3d636567a15d95908
GabrielAceti <gabriel.aceti@hotmail.com>
2021-04-19 08:53:52 -0300

    Alteração do caminho do arquivo de rotas

--- servidor/server.js
+++ servidor/server.js
@@ -2,7 +2,7 @@ const cors = require('cors')
 const {json} = require('express');
 const express = require('express');
 const app = express();
-const routes = require('../src/routes');
+const routes = require('./routes');
 
 app.use(express.json());
 app.use(cors())
